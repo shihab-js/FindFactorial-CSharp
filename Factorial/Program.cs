1ce86c962fb6f72fff1ee2872ebd53689426d67c
Md. Shihab <shihabrayhan8@gmail.com>
2021-09-22 11:36:42 +0600

    Variables declared!

--- Factorial/Program.cs
+++ Factorial/Program.cs
@@ -6,7 +6,13 @@ namespace Factorial
     {
         static void Main(string[] args)
         {
-            
+            // 4! = 4*3*2*1
+            // 6! = 6*5*4*3*2*1
+
+            //declare the necessary variables
+            int i, fact = 1, number;
+
+
         }
     }
 }
